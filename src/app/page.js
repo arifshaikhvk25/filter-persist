59eca40a507ce0056d705db2fa2cd0effdc91687
arifshaikhvk25 <arifshaikhvk25@gmail.com>
2024-07-27 17:45:05 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Filter from "./filter/Filter";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>Filter Persist</div>
+      {/* <div>Filter Persist</div> */}
+      <Filter />
     </main>
   );
 }
